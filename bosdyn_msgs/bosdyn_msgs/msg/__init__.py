71a4721010f170c7a36caf1c23750b9d719247d1
mhidalgo-bdai <144129882+mhidalgo-bdai@users.noreply.github.com>
2024-02-20 15:49:47 -0300

    Bump Spot SDK version from 3.3.2 to 4.0.0 (#30)
    * Bump Spot SDK version from 3.3.2 to 4.0.0
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>
    
    * Include GPS messages in bosdyn_api_msgs
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>
    
    * Add bosdyn_metrics_logging_api_msgs package
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>
    
    * Update proto2ros config overlays
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>
    
    * Update ros_utilities submodule
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>
    
    * Update ros_utilities submodule
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>
    
    ---------
    
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bosdyn_msgs/bosdyn_msgs/msg/__init__.py
+++ bosdyn_msgs/bosdyn_msgs/msg/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Boston Dynamics AI Institute LLC. All rights reserved.
+# Copyright (c) 2024 Boston Dynamics AI Institute LLC. All rights reserved.
 
 from bosdyn_api_msgs.msg import *  # noqa
 from bosdyn_auto_return_api_msgs.msg import *  # noqa
@@ -6,6 +6,7 @@ from bosdyn_autowalk_api_msgs.msg import *  # noqa
 from bosdyn_graph_nav_api_msgs.msg import *  # noqa
 from bosdyn_keepalive_api_msgs.msg import *  # noqa
 from bosdyn_log_status_api_msgs.msg import *  # noqa
+from bosdyn_metrics_logging_api_msgs.msg import *  # noqa
 from bosdyn_mission_api_msgs.msg import *  # noqa
 from bosdyn_spot_api_msgs.msg import *  # noqa
 from bosdyn_spot_cam_api_msgs.msg import *  # noqa
